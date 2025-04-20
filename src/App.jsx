5327f79f53eb267a63b307e4a4cab6fd4ecddc1e
Dustymon111 <dasutinraioneru@gmail.com>
2025-04-20 12:59:15 +0700

    website apple clone done

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@ import Navbar from "./components/Navbar"
 import Hero from "./components/Hero"
 import Highlights from "./components/Highlights"
 import Model from "./components/Model"
+import Feature from "./components/Feature"
+import HowItWorks from "./components/HowItWorks"
+import Footer from "./components/Footer"
 
 
 const App = () => {
@@ -11,6 +14,9 @@ const App = () => {
       <Hero />
       <Highlights />
       <Model />
+      <Feature />
+      <HowItWorks />
+      <Footer />
     </main>
   )
 }
